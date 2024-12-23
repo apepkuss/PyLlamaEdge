171934ae4c8ac1acebde03d3a043917adf640078
Xin Liu <sam@secondstate.io>
2024-12-23 16:41:42 +0800

    refactor: improve client
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- tests/test_client.py
+++ tests/test_client.py
@@ -10,8 +10,15 @@ class TestChat(unittest.TestCase):
             messages=[{"role": "user", "content": "What is the capital of France?"}]
         )
         assistant_message = response["choices"][0]["message"]["content"]
-        print(assistant_message)
+        # print(assistant_message)
         self.assertTrue("Paris" in assistant_message)
+
+    def test_whisper(self):
+        client = Client(server_base_url="http://localhost:8080")
+        wav_file = "./tests/assets/test.wav"
+        text = client.transcribe(wav_file)
+        # print(text)
+        self.assertTrue("This is a test record for whisper.cpp" in text)
 
 
 if __name__ == "__main__":
