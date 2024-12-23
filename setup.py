93f90dd67d329d4d0daa58d4ec0426d59448994a
Xin Liu <sam@secondstate.io>
2024-12-23 17:14:52 +0800

    chore: update `setup.py`
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import find_packages, setup
 
+# Add these lines at the top
+with open("requirements.txt") as f:
+    requirements = [
+        line.strip() for line in f if line.strip() and not line.startswith("#")
+    ]
+
 setup(
     name="llamaedge",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=[
-        # 在这里添加依赖
-    ],
+    install_requires=requirements,
     author="Liu Xin",
     author_email="xin.sam.liu@hotmail.com",
     description="Python library for LlamaEdge",
