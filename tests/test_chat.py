f5d1633778ef940121e5ffed9b9f433eadd00e50
Xin Liu <sam@secondstate.io>
2024-12-23 16:30:18 +0800

    test: add `test_chat.py`
    Signed-off-by: Xin Liu <sam@secondstate.io>

---
+++ tests/test_chat.py
@@ -0,0 +1,18 @@
+import unittest
+
+from llamaedge.client import Client
+
+
+class TestChat(unittest.TestCase):
+    def test_chat(self):
+        client = Client(server_base_url="http://localhost:12306")
+        response = client.chat(
+            messages=[{"role": "user", "content": "What is the capital of France?"}]
+        )
+        assistant_message = response["choices"][0]["message"]["content"]
+        print(assistant_message)
+        self.assertTrue("Paris" in assistant_message)
+
+
+if __name__ == "__main__":
+    unittest.main()
