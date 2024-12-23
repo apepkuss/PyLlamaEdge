e7429d53459d31b0a211a61137476375b94c3e31
Xin Liu <sam@secondstate.io>
2024-12-23 17:27:19 +0800

    chore: update `setup.py`
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     name="llamaedge",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=requirements,
+    install_requires=["requests==2.32.3"],
     author="Liu Xin",
     author_email="xin.sam.liu@hotmail.com",
     description="Python library for LlamaEdge",
@@ -18,9 +18,13 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/apepkuss/PyLlamaEdge",
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
     python_requires=">=3.11",
 )
